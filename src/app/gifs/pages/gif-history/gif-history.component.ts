10edc15cc71753d048c2e7edb6effb4bde066d2c
Tío Gallo <juanftp100@gmail.com>
2025-12-09 15:25:21 -0300

    uso del service para manejar el componente history

--- src/app/gifs/pages/gif-history/gif-history.component.ts
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -1,11 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
+import { GifService } from '../../services/gifs.services';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
 
 @Component({
   selector: 'app-gif-history',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './gif-history.component.html'
 })
 export default class GifHistoryComponent {
@@ -13,8 +15,14 @@ export default class GifHistoryComponent {
   // query = inject(ActivatedRoute).params.subscribe(
   //   params => console.log( params['query'] )
   // );
+  gifService = inject(GifService);
+
   query = toSignal(
     inject(ActivatedRoute).params.pipe(map( params => params['query']))
   );
+
+  gifsByKey = computed(() => {
+    return this.gifService.getHistoryGifs(this.query());
+  });
 
 }
--- src/app/gifs/services/gifs.services.ts
+++ src/app/gifs/services/gifs.services.ts
@@ -5,7 +5,7 @@ import { environment } from '@environments/environment';
 import { Gif } from '../interfaces/gif.interface';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -37,7 +37,7 @@ export class GifService {
 
   }
 
-  searchGifs(query: string) {
+  searchGifs(query: string): Observable<Gif[]> {
     return this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/search`, {
       params: {
         api_key: environment.giphyApiKey,
@@ -67,6 +67,10 @@ export class GifService {
     //   this.trendingGifs.set(gifs);
 
     // });
+  }
+
+  getHistoryGifs(query: string): Gif[] {
+    return this.searchHistory()[query] ?? [];
   }
 
 }
