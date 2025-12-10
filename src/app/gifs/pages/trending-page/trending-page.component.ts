cc8e3cde120929b45b89eddbe622030c1090084d
Tío Gallo <juanftp100@gmail.com>
2025-12-10 13:54:44 -0300

    implementacion del masonry

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -5,7 +5,6 @@ import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifListComponent],
   templateUrl: './trending-page.component.html'
 })
 export default class TrendingPageComponent {
--- src/app/gifs/services/gifs.services.ts
+++ src/app/gifs/services/gifs.services.ts
@@ -20,6 +20,15 @@ export class GifService {
 
   trendingGifs = signal<Gif[]>([]);
 
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    for( let i = 0; i < this.trendingGifs().length; i += 3 ) {
+      groups.push(this.trendingGifs().slice(i, i + 3));
+    }
+
+    return groups;
+  });
+
   searchHistory = signal<Record<string, Gif[]>>(loadFromStorage())
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
